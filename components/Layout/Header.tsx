2a4e1a6bd1d690af158301cdd0b452c305d2b692
justinschmitz97 <justin.schmitz97@gmail.com>
2021-10-20 17:28:18 +0200

    Over Header Support

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -3,8 +3,8 @@ import NextLink from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function Header() {
-  /* Detect scroll for fixed header */
   const [isFixed, setIsFixed] = useState(true);
+  const [support, setSupport] = useState(false);
   const handleScroll = () => {
     window.pageYOffset > 60 ? setIsFixed(true) : setIsFixed(false);
   };
@@ -14,9 +14,17 @@ export default function Header() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  new Promise(() => {
+    const image = new Image();
+    image.onerror = () => setSupport(false);
+    image.onload = () => setSupport(true);
+    image.src =
+      "data:image/jxl;base64,/woIELASCAgQAFwASxLFgkWAHL0xqnCBCV0qDp901Te/5QM=";
+  }).catch(() => false);
+
   return (
     <header
-      className={`py-1 px-3 fixed w-100 right-0 left-0 top-0 z-50 ${
+      className={`fixed w-100 right-0 left-0 top-0 z-50 ${
         isFixed ? "fixed-header" : undefined
       }`}
       style={
@@ -26,9 +34,18 @@ export default function Header() {
               backdropFilter: "blur(4px)",
               zIndex: 9999,
             }
-          : { zIndex: 9999 }
+          : {}
       }
     >
+      <div
+        className={`block text-center text-tiny p-1 ${
+          isFixed ? "hidden" : ""
+        } ${support ? "bg-gradient" : "bg-bg-200"}`}
+      >
+        {support
+          ? "Your browser supports JPEG XL.🥳"
+          : "Your browser does not support JPEG XL.😞"}
+      </div>
       <div className="flex flex-wrap justify-between items-center md:flex-row">
         <nav className="flex flex-wrap justify-center items-center text-base md:pl-4 md:mr-auto">
           <Link className="block p-2 md:p-3" text="Blog" href="/#blog" />
