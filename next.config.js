0b3aa282625b1254e4e8b43f9ec02c1577891bbc
justinschmitz97 <justin.schmitz97@gmail.com>
2021-10-26 13:08:09 +0200

    config fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 /* eslint-disable no-undef */
 const withPlugins = require("next-compose-plugins");
 const images = require("next-images");
+const bundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const baseUrl = "";
 
 const nextConfig = {
   eslint: {
@@ -13,7 +18,13 @@ module.exports = withPlugins(
   [
     images,
     [
+      bundleAnalyzer,
       {
+        trailingSlash: true,
+        basePath: baseUrl,
+        env: {
+          baseUrl: baseUrl,
+        },
         webpack: (config, { dev, isServer }) => {
           config.module.rules.push({
             test: /\.svg$/,
